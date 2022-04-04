40be543e54c8cbeb7aab232d2cbf88a5e36e7af9
raphael-andrade <raphael-andrade@live.com>
2022-04-04 20:55:03 -0300

    Payment, one-to-one association

--- src/main/java/com/udemyexercise/ExerciseUdemy/config/TestConfig.java
+++ src/main/java/com/udemyexercise/ExerciseUdemy/config/TestConfig.java
@@ -71,6 +71,9 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3,p5,2,p5.getPrice());
 
         orderItemRepositories.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+        Payment pay1 = new Payment(null,Instant.parse("2019-06-20T19:53:07Z"),o1 );
+        o1.setPayment(pay1);
 
+        orderRepositories.save(o1);
     }
 }
